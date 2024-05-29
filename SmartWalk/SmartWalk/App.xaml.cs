b7a434ebfc590eb659d1db67b0b57133370b6f6a
星 優太 <youtaixing753@gmail.com>
2024-05-29 13:42:01 +0900

    はじめの一歩

---
+++ SmartWalk/SmartWalk/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartWalk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SmartWalk/SmartWalk/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartWalk
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
