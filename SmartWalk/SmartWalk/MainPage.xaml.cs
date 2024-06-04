0a36d6c0bd6d6ac101386d7900b71d4c3f819f4a
星 優太 <youtaixing753@gmail.com>
2024-06-04 11:17:23 +0900

    進捗めちゃ良き

--- SmartWalk/SmartWalk/MainPage.xaml.cs
+++ SmartWalk/SmartWalk/MainPage.xaml.cs
@@ -2,7 +2,20 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        String[] mame5 =new string[] { 
+            "切れた電池は擦れば復活する",
+        "エアコンの室外機を日光に当たらないようにするだけで電気代を節約することができる。" ,
+        "缶詰のオイルやサラダ油で簡易的なランプをつくることができる",
+        "硬い肉はコーラなどの炭酸飲料で柔らかくなる",
+        "こびりついた油汚れはレモンで楽に落とせる",
+        "歯を磨くとき、歯ブラシは濡らさない方がより綺麗に磨ける。",
+        "掛け布団の上に毛布をかけた方が温かい。"
+
+        };
+        String mame4 = "";
+        String mame3 = "";
+        String mame2 = "";
+        String mame1 = "";
 
         public MainPage()
         {
@@ -11,7 +24,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+         
 
            
         }
--- SmartWalk/SmartWalk/obj/Debug/net8.0-ios/iossimulator-x64/SmartWalk.AssemblyInfo.cs
+++ SmartWalk/SmartWalk/obj/Debug/net8.0-ios/iossimulator-x64/SmartWalk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartWalk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f21fea3552332c5614a2d337220a6b0a66e36ed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a4e818a654d2743aba2b62c146c51c5bb1bf32de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartWalk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartWalk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
